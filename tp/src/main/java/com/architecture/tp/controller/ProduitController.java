53ed2409eed14b709df5fd3979edb59ba66ffd7f
eyakm <eyakammoun20@gmail.com>
2024-11-06 00:34:10 +0100

    Ajout de la fonctionnalité d'affichage des détails d'un produit spécifique

--- tp/src/main/java/com/architecture/tp/controller/ProduitController.java
+++ tp/src/main/java/com/architecture/tp/controller/ProduitController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,4 +27,11 @@ public class ProduitController {
  produitService.save(produit);
  return "redirect:/produits"; // Rediriger vers la liste des produits
  }
+
+    @GetMapping("/details/{id}")
+    public String productDetails(@PathVariable Long id, Model model) {
+        Produit produit = produitService.findById(id);
+        model.addAttribute("produit", produit);
+        return "details";
+    }
 }
