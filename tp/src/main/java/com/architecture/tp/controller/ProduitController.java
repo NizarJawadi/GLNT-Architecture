89d1486ebf66bb33d0241dab09de80674f80a7d6
eyakm <eyakammoun20@gmail.com>
2024-11-10 22:16:21 +0100

    Ajout de la fonctionnalité de récupérer les détails d'un produit spécifique par son ID et suppression

--- tp/src/main/java/com/architecture/tp/controller/ProduitController.java
+++ tp/src/main/java/com/architecture/tp/controller/ProduitController.java
@@ -1,8 +1,11 @@
 package com.architecture.tp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +63,12 @@ public class ProduitController {
             produitService.save(produit); // Enregistre les modifications
         }
         return "redirect:/produits"; // Redirige vers la liste des produits aprÃ¨s modification
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        produitService.delete(id);
+        return "redirect:/produits";
     }
 
 }
